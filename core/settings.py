73654facdffebce4f38483946ef48a5e65e474b7
AnkitMalvi1 <ankitmalviya06219@gmail.com>
2025-02-02 16:57:16 +0530

    update .gitignore and modify settings for production readiness

--- core/settings.py
+++ core/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_&v=cknihuh7au@y%x))pcayl*%7*-b*71injubrvapx5dqb^!'
+SECRET_KEY = 'my_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
